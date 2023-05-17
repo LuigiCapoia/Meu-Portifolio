858396c2b32fe92c905f9cbaefa9768200858f2b
unknown <luigicapoia@gmail.com>
2023-05-17 20:00:18 -0300

    Atualizacao do script

---
+++ script.js
@@ -0,0 +1,9 @@
+var Maario = document.getElementById("mario")
+var React = document.getElementById("react")
+
+Maario.addEventListener("click", function () {
+    window.location = 'https://github.com/LuigiCapoia/MarioJump';
+});
+React.addEventListener("click", function () {
+    window.location = 'https://github.com/LuigiCapoia/TI-Academy-React';
+});
