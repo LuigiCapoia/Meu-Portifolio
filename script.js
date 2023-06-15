923916a604cfd60442aaa6955612376d5f3eea72
LuigiCapoia <luigicapoia@gmail.com>
2023-06-15 16:18:22 -0300

    portifolio

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var Maario = document.getElementById("mario")
 var React = document.getElementById("react")
-
+var Native = document.getElementById("react-native")
+var HtmlCss = document.getElementById("html")
 Maario.addEventListener("click", function () {
     window.location = 'https://github.com/LuigiCapoia/MarioJump';
 });
 React.addEventListener("click", function () {
     window.location = 'https://github.com/LuigiCapoia/TI-Academy-React';
+});
+Native.addEventListener("click", function () {
+    window.location = 'https://github.com/LuigiCapoia/Aplicativo_De_Financas';
+});
+HtmlCss.addEventListener("click", function () {
+    window.location = 'https://github.com/LuigiCapoia/ciclo1-html-css';
 });
